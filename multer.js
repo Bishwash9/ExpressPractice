35ff1e80010e41f37526e16347d54e32759370d5
Bishwas <bishwasmahajan@gmail.com>
2026-01-25 20:56:32 +0545

    feat:Defined multer and added basic file validation for uploading files

--- multer.js
+++ multer.js
@@ -4,6 +4,20 @@ const app = express();
 
 app.use(express.json());
 
+//this js is for uploading file through third party called multer
+const multer = require('multer');
+
+const fileFilter = ((req,file,cb)=>{
+    if(file.mimetype === 'image/jpg' || file.mimetype === 'image/png'){
+        cb(null,true);
+    }else{
+        cb(new Error('Only jpg and png file is allowed'),false);
+    }
+});
+
+
+
+
 app.listen(port,()=>{
     console.log(`Started server at port ${port}`);
 });
