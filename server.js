fb11214ce4be925af2ee78b80f50c0483bb6f245
Bishwas <bishwasmahajan@gmail.com>
2026-01-27 23:27:27 +0545

    feat:added a category router on server

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const port = 4000;
 let productRouter = require('./Routes/productRoutes');
 app.use('/products',productRouter)
 
+let categoryRouter = require('./Routes/categoryRoutes');
+app.use('/categories',categoryRouter)
+
 app.listen(port,()=>{
     console.log(`Server Started at ${port}`);
 })
