ce6b75be45a448936c6e5acc685e72243a26f5aa
Bishwas <bishwasmahajan@gmail.com>
2026-01-27 23:31:56 +0545

    feat:added router get for getting json data of categories

--- Routes/categoryRoutes.js
+++ Routes/categoryRoutes.js
@@ -4,3 +4,7 @@ const router = express.Router();
 router.use(express.json());
 
 let categoryList = ['CLoth', 'Home', 'Electronics'];
+
+router.get('/categoryList', (req,res)=>{
+    res.json(categoryList)
+});
