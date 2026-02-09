bc4cce7e22660bcc40dd164cff279b0541e46da1
Bishwas <bishwasmahajan@gmail.com>
2026-02-09 23:53:41 +0545

    Another

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = requiure('express');
 const app = express();
 const port = 4000;
 
+//product routes
 let productRouter = require('./Routes/productRoutes');
 app.use('/products',productRouter)
 
+//category routes
 let categoryRouter = require('./Routes/categoryRoutes');
 app.use('/categories',categoryRouter)
 
